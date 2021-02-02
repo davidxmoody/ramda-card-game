ca4ddd2df5de5e302891867d2eb28cd651483231
David Moody <david@davidxmoody.com>
2021-02-02 19:56:45 +0000

    Add previously uncommitted tweaks

--- game.ts
+++ game.ts
@@ -5,61 +5,61 @@ function randomPick<T>(list: T[], num: number): T[] {
   return R.sortBy(() => Math.random(), list).slice(0, num)
 }
 
-// const startingConstants = randomPick(R.range(-20, 21), 3)
-// const targetConstants = randomPick(R.range(-20, 21), 3)
+const startingConstants = randomPick(R.range(0, 11), 3)
+const targetConstants = randomPick(R.range(0, 11), 3)
 // const startingConstants = randomPick(R.range(-10, 11), 1)
 // const targetConstants = randomPick(R.range(-10, 11), 1)
-const startingConstants = [
-  randomPick(R.range(0, 6), Math.ceil(Math.random() * 3)),
-]
-const targetConstants = [
-  randomPick(R.range(0, 6), Math.ceil(Math.random() * 3)),
-]
+// const startingConstants = [
+//   randomPick(R.range(0, 6), Math.ceil(Math.random() * 3)),
+// ]
+// const targetConstants = [
+//   randomPick(R.range(0, 6), Math.ceil(Math.random() * 3)),
+// ]
 
-const handConstants = [...R.range(0, 6), ...R.range(-3, 6)]
+const handConstants = randomPick([...R.range(0, 6), ...R.range(-3, 11)], 5)
 
 const mathFunctionNames: Array<keyof typeof R> = [
   "add",
+  "subtract",
   "always",
   "dec",
-  "divide",
   "inc",
+  // "divide",
   // "mathMod", // Too similar to mathMod
   // "mean",
   // "median",
-  "modulo",
-  "multiply",
-  "negate",
+  // "modulo",
+  // "multiply",
+  // "negate",
   // "product",
-  "subtract",
   // "sum",
-  "clamp",
-  "min",
-  "max",
+  // "clamp",
+  // "min",
+  // "max",
 ]
 
-const numberListFunctionNames: Array<keyof typeof R> = [
-  "adjust", "adjust", "adjust", "adjust",
-  "append", "append", "append", "append",
-  "drop",
-  "dropLast",
-  "dropRepeats",
-  "init",
-  "insert",
-  "intersperse",
-  "map",
-  "move",
-  "remove",
-  "reverse",
-  "slice",
-  "tail",
-  "take",
-  "takeLast",
-  "uniq",
-  "update", "update",
-]
+// const numberListFunctionNames: Array<keyof typeof R> = [
+//   "adjust", "adjust", "adjust", "adjust",
+//   "append", "append", "append", "append",
+//   "drop",
+//   "dropLast",
+//   "dropRepeats",
+//   "init",
+//   "insert",
+//   "intersperse",
+//   "map",
+//   "move",
+//   "remove",
+//   "reverse",
+//   "slice",
+//   "tail",
+//   "take",
+//   "takeLast",
+//   "uniq",
+//   "update", "update",
+// ]
 
-let hand = randomPick([...mathFunctionNames, ...numberListFunctionNames, ...handConstants], 3)
+let hand = randomPick([...mathFunctionNames, ...handConstants], 3)
 
 function printHand() {
   console.log("Starting constants:")
@@ -85,9 +85,11 @@ const rl = readline.createInterface({
 
 rl.on("line", () => {
   hand = hand.concat(
-    randomPick(R.difference([...mathFunctionNames, ...numberListFunctionNames, ...handConstants], hand), 1),
+    randomPick(R.difference([...mathFunctionNames, ...handConstants], hand), 1),
   )
   printHand()
 })
 
 printHand()
+
+
